4acad3a4dee9d751c2d9152a14844783bc303b37
Aleksandar Drakaliyski <theall3xander@gmail.com>
2023-07-07 14:49:47 +0100

    The images are now also rendered dynamically

--- static/js/features.js
+++ static/js/features.js
@@ -2,32 +2,32 @@ document.addEventListener("DOMContentLoaded", function() {
     const features = [
         {
             id: 1,
-            image: "{% static 'images/features/f1.png' %}",
+            image: "static/images/features/f1.png",
             title: "Free Shipping",
         },
         {
             id: 2,
-            image: "{% static 'images/features/f2.png' %}",
+            image: "static/images/features/f2.png",
             title: "Online Order",
         },
         {
             id: 3,
-            image: "{% static 'images/features/f3.png' %}",
+            image: "static/images/features/f3.png",
             title: "Save Money",
         },
         {
             id: 4,
-            image: "{% static 'images/features/f4.png' %}",
+            image: "static/images/features/f4.png",
             title: "Happy Sell",
         },
         {
             id: 5,
-            image: "{% static 'images/features/f5.png' %}",
+            image: "static/images/features/f5.png",
             title: "F24/7 Support",
         },
         {
             id: 6,
-            image: "{% static 'images/features/f6.png' %}",
+            image: "static/images/features/f6.png",
             title: "Free Shipping",
         },
     ]
