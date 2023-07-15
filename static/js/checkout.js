1f27bf8e2aa9865f4ef907b11929f1b063ba7da6
Aleksandar Drakaliyski <theall3xander@gmail.com>
2023-07-15 15:33:49 +0100

    Workoed on checkout page but need to fix a bug

--- static/js/checkout.js
+++ static/js/checkout.js
@@ -3,6 +3,7 @@ var submitBtn = document.getElementById('submit-btn')
 var paymentInfo = document.getElementById('payment-info')
 var payBtn = document.getElementById('make-payment')
 var userInfo = document.getElementById('user-info')
+var total = '{{order.get_total|floatformat:2}}'
 
 if (user != 'AnonymousUser' && userInfo != null ) {
     userInfo.innerHTML = ''
@@ -24,5 +25,29 @@ if (form != null) {
     function submitForm(e){
         console.log('Payment successful!');
         alert('Payment successful!')
+
+        var userForm = {
+            'name':null,
+            'email':null,
+            'total':total,
+        }
+
+        var shippingInfo = {
+            'address': null,
+            'city': null,
+            'postcode': null,
+        }
+
+        if (user == 'AnonymousUser'){
+            userForm.name = form.name.value
+            userForm.email = form.email.value
+        }
+
+        shippingInfo.address = form.address.value
+        shippingInfo.city = form.city.value
+        shippingInfo.postcode = form.postcode.value
+
+        console.log('User Info: ', userForm);
+        console.log('Shipping Info: ', shippingInfo);
     }
 }
