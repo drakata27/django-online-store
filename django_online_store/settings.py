2d3f364b0d57c7d57825f21a7991e73e4b33963d
Aleksandar Drakaliyski <theall3xander@gmail.com>
2023-09-23 20:28:55 +0100

    Made small changes

--- django_online_store/settings.py
+++ django_online_store/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-a^cj)t32cii=$0xgl(+2z4dvmb06(=@%!ysa(qahz&i!l0+pf)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
