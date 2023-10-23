1436a955e446111e5cb81f3e05c8be6a94b2a3c7
drakata27 <theall3xander@gmail.com>
2023-10-23 18:16:24 +0100

    Preparing for deployment

--- django_online_store/settings.py
+++ django_online_store/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a^cj)t32cii=$0xgl(+2z4dvmb06(=@%!ysa(qahz&i!l0+pf)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 
 # Application definition
